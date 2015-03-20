65eeefc1d78b1062e7daee3c3d1be14cac0ef425
Luo Sheng <luosheng1986@gmail.com>
2015-03-20 11:46:57 +0800

    Add project file.

---
+++ DOUAudioStreamer/DOUAudioStreamer.h
@@ -0,0 +1,19 @@
+//
+//  DOUAudioStreamer.h
+//  DOUAudioStreamer
+//
+//  Created by Luo Sheng on 3/20/15.
+//  Copyright (c) 2015 Douban. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for DOUAudioStreamer.
+FOUNDATION_EXPORT double DOUAudioStreamerVersionNumber;
+
+//! Project version string for DOUAudioStreamer.
+FOUNDATION_EXPORT const unsigned char DOUAudioStreamerVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <DOUAudioStreamer/PublicHeader.h>
+
+
